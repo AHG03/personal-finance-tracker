bb95e033321278f8ee702bf1d7ba63cf8d4ae92d
Ali Ahangar <aliahangar80@gmail.com>
2026-01-14 22:49:04 +0330

    Add persistant using json

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
-class Transaction():
-    def __init__(self, amount, kind, category, note):
+import json
+
+
+class Transaction:
+    def __init__(self, amount, transaction_type, category, note):
         self.amount = amount
-        self.kind = kind
+        self.transaction_type = transaction_type
         self.category = category
         self.note = note
 
 
-class FinanceManager():
+class FinanceManager:
     def __init__(self):
         self.transactions = []
 
-    def add_transaction(self, amount, kind, category, note):
-        transaction = Transaction(amount, kind, category, note)
+
+    def add_transaction(self, amount, transaction_type, category, note):
+        transaction = Transaction(amount, transaction_type, category, note)
         self.transactions.append(transaction)
 
 
     def show_transactions(self):
         for transaction in self.transactions:
-            print(f"Amount: {transaction.amount}, kind: {transaction.kind}, Category: {transaction.category}, Note: {transaction.note}")
+            print(f"Amount: {transaction.amount}, transaction_type: {transaction.transaction_type}, Category: {transaction.category}, Note: {transaction.note}")
+
+
+    def load_from_file(self):
+        try:
+            with open("transactions.json", 'r') as file:
+                data = json.load(file)
+                self.transactions = []
+                for item in data:
+                    transaction = Transaction(item["amount"], item["transaction_type"], item["category"], item["note"])
+                    self.transactions.append(transaction)
+        except FileNotFoundError:
+            with open("transactions.json", 'w') as file:
+                json.dump([], file)
+
+
+    def save_to_file(self):
+        data = []
+        for item in self.transactions:
+            data.append({"amount": item.amount, "transaction_type": item.transaction_type, "category": item.category, "note": item.note})
+        with open("transactions.json", 'w') as file:
+            json.dump(data, file, indent=4)
 
 
 def main():
     finance_manager = FinanceManager()
+    finance_manager.load_from_file()
+
     while True:
         print("\nWelcome to my personal finance tracker")
         print("1. Add transaction")
@@ -31,11 +58,12 @@ def main():
         choice = input("Please enter your choice: ")
         if choice == "1":
             amount = float(input("Please enter the amount: $"))
-            kind = input("Please enter the transaction kind (income/expense):")
+            transaction_type = input("Please enter the transaction type (income/expense): ")
             category = input("Please enter the transaction category: ")
             note = input("Please enter the transaction note: ")
 
-            finance_manager.add_transaction(amount, kind, category, note)
+            finance_manager.add_transaction(amount, transaction_type, category, note)
+            finance_manager.save_to_file()
 
         elif choice == "2":
             finance_manager.show_transactions()
